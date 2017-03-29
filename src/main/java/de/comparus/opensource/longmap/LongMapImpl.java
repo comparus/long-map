8fd8ad8bc37c54c799185b810cdef1d7c298b80e
Volodymyr Gadiatskyi <v.gadiatskyi@comparus.de>
2017-03-29 12:58:01 +0200

    first commit

---
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -0,0 +1,43 @@
+package de.comparus.opensource.longmap;
+
+public class LongMapImpl<V> implements LongMap<V> {
+    public V put(long key, V value) {
+        return null;
+    }
+
+    public V get(long key) {
+        return null;
+    }
+
+    public V remove(long key) {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean containsKey(long key) {
+        return false;
+    }
+
+    public boolean containsValue(V value) {
+        return false;
+    }
+
+    public long[] keys() {
+        return null;
+    }
+
+    public V[] values() {
+        return null;
+    }
+
+    public long size() {
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+}
