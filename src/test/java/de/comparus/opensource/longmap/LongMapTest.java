4c2bde7aae5b5603e9eceffcbc70a85d4ca4d12f
Evgeniy Andrienko <stelionser@gmail.com>
2020-08-30 17:46:59 +0300

    Prepared project to LongMap implementation.

---
+++ src/test/java/de/comparus/opensource/longmap/LongMapTest.java
@@ -0,0 +1,14 @@
+package de.comparus.opensource.longmap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongMapTest {
+
+    @Test
+    void testCase() {
+        assertTrue(true);
+    }
+
+}
