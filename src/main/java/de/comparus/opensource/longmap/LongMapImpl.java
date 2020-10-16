03eaa7191d4a6db8abbcc73ba253cfa452e5c847
Horus-Lupercal <sg.wofl@gmail.com>
2020-10-16 22:29:45 +0300

    finished Impl for LongMap

--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -1,43 +1,320 @@
 package de.comparus.opensource.longmap;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.StringJoiner;
+
 public class LongMapImpl<V> implements LongMap<V> {
-    public V put(long key, V value) {
-        return null;
+
+  private static final int DEFAULT_INITIAL_CAPACITY = 16;
+  private static final int MAXIMUM_CAPACITY = 1 << 30;
+  private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+  private int size;
+  private Node<V>[] table;
+
+  private final float loadFactor;
+  private int threshold;
+
+  public LongMapImpl() {
+    this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
+  }
+
+  public LongMapImpl(int initialCapacity) {
+    this(initialCapacity, DEFAULT_LOAD_FACTOR);
+  }
+
+  public LongMapImpl(int initialCapacity, float loadFactor) {
+
+    if (initialCapacity < 0) {
+      throw new IllegalArgumentException("Initial capacity must be of power 2: " + initialCapacity);
     }
 
-    public V get(long key) {
-        return null;
+    if (initialCapacity > MAXIMUM_CAPACITY) {
+      initialCapacity = MAXIMUM_CAPACITY;
     }
 
-    public V remove(long key) {
-        return null;
+    if (loadFactor <= 0 || Float.isNaN(loadFactor)) {
+      throw new IllegalArgumentException("Illegal load factor: " + loadFactor);
     }
 
-    public boolean isEmpty() {
-        return false;
+    int capacity = 1;
+    while (capacity < initialCapacity) {
+      capacity <<= 1;
     }
 
-    public boolean containsKey(long key) {
-        return false;
+    table = newTable(capacity);
+    this.loadFactor = loadFactor;
+    threshold = (int) (capacity * loadFactor);
+  }
+
+  public Node<V>[] getTableForTest() {
+    return table;
+  }
+
+  public V put(long key, V value) {
+    int h = hash(key);
+    Node<V>[] node = getTable();
+
+    for (Node<V> n = node[h]; n != null; n = n.next) {
+      if (h == n.hash && key == n.key) {
+        V oldValue = n.value;
+
+        if (value != oldValue) {
+          n.value = value;
+        }
+
+        return oldValue;
+      }
+    }
+    Node<V> n = node[h];
+    node[h] = new Node<>(h, key, value, n);
+
+    if (++size >= threshold) {
+      resize(node.length * 2);
+    }
+    return value;
+  }
+
+  public V get(long key) {
+
+    int h = hash(key);
+    Node<V>[] tab = getTable();
+    Node<V> n = tab[h];
+    while (n != null) {
+      if (n.hash == h && key == n.key) {
+        return n.value;
+      }
+      n = n.next;
+    }
+    return null;
+  }
+
+  public V remove(long key) {
+    int h = hash(key);
+    Node<V>[] tab = getTable();
+    Node<V> prev = tab[h];
+    Node<V> node = prev;
+
+    while (node != null) {
+      Node<V> next = node.next;
+      if (h == node.hash && node.key == key) {
+        size--;
+        if (prev == node) {
+          tab[h] = next;
+        } else {
+          prev.next = next;
+        }
+        return node.value;
+      }
+      prev = node;
+      node = next;
+    }
+    return null;
+  }
+
+  public boolean isEmpty() {
+    return size() == 0;
+  }
+
+  public boolean containsKey(long key) {
+    Node<V>[] tab = getTable();
+
+    for (int i = 0; i < tab.length; i++) {
+      for (Node<V> n = tab[i]; n != null; n = n.next) {
+        if (key == n.key) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public boolean containsValue(V value) {
+    if (value == null) {
+      return containsNullValue();
     }
 
-    public boolean containsValue(V value) {
-        return false;
+    Node<V>[] tab = getTable();
+
+    for (int i = 0; i < tab.length; i++) {
+      for (Node<V> n = tab[i]; n != null; n = n.next) {
+        if (value.equals(n.value)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public long[] keys() {
+    List<Long> list = new ArrayList<>();
+    for (Node<V> vNode : table) {
+      Node<V> node = vNode;
+      while (node != null) {
+        list.add(node.getKey());
+        node = node.next;
+      }
     }
 
-    public long[] keys() {
-        return null;
+    long[] array = new long[list.size()];
+    for (int i = 0; i < list.size(); i++) {
+      array[i] = list.get(i);
+    }
+    return array;
+  }
+
+  public V[] values() {
+    List<V> list = new ArrayList<>();
+    for (Node<V> vNode : table) {
+      Node<V> node = vNode;
+      while (node != null) {
+        list.add(node.getValue());
+        node = node.next;
+      }
     }
 
-    public V[] values() {
-        return null;
+    @SuppressWarnings("unchecked")
+    V[] result = (V[]) Array.newInstance(list.get(0).getClass(), list.size());
+    for (int i = 0; i < list.size(); i++) {
+      result[i] = list.get(i);
+    }
+    return result;
+  }
+
+  public long size() {
+    return size;
+  }
+
+  public void clear() {
+    Node<V>[] node;
+    if ((node = table) != null && size > 0) {
+      size = 0;
+      for (int i = 0; i < node.length; i++) {
+        node[i] = null;
+      }
+    }
+  }
+
+  @Override
+  public String toString() {
+    StringJoiner sj = new StringJoiner(System.lineSeparator());
+
+    Node<V>[] tab = getTable();
+    for (int i = 0; i < tab.length; i++) {
+      for (Node<V> n = tab[i]; n != null; n = n.next) {
+        sj.add(n.toString());
+      }
+    }
+    return sj.toString();
+  }
+
+  private boolean containsNullValue() {
+    Node<V>[] tab = getTable();
+
+    for (int i = tab.length - 1; i > 0; i--) {
+      for (Node<V> n = tab[i]; n != null; n = n.next) {
+        if (n.value == null) {
+          return true;
+        }
+      }
     }
 
-    public long size() {
-        return 0;
+    return false;
+  }
+
+  private int hash(Object key) {
+    return key.hashCode() % table.length;
+  }
+
+  private void resize(int newCapacity) {
+    Node<V>[] oldTable = getTable();
+    int oldCapacity = oldTable.length;
+    if (oldCapacity == MAXIMUM_CAPACITY) {
+      threshold = Integer.MAX_VALUE;
+      return;
     }
 
-    public void clear() {
+    Node<V>[] newTable = newTable(newCapacity);
+    transfer(oldTable, newTable);
+    table = newTable;
 
+    if (size >= threshold / 2) {
+      threshold = (int) (newCapacity * loadFactor);
+    } else {
+      transfer(newTable, oldTable);
+      table = oldTable;
     }
+  }
+
+  private void transfer(Node<V>[] src, Node<V>[] dest) {
+    for (int i = 0; i < src.length; i++) {
+      Node<V> n = src[i];
+      src[i] = null;
+      while (n != null) {
+        Node<V> next = n.next;
+        if (n.key == null) {
+          n.next = null;
+          n.value = null;
+          size--;
+        } else {
+          n.next = dest[n.hash];
+          dest[n.hash] = n;
+        }
+        n = next;
+      }
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private Node<V>[] newTable(int capacity) {
+    return (Node<V>[]) new Node<?>[capacity];
+  }
+
+  private Node<V>[] getTable() {
+    return table;
+  }
+
+  static class Node<V> {
+    final int hash;
+    final Long key;
+    V value;
+    Node<V> next;
+
+    Node(int hash, Long key, V value, Node<V> next) {
+      this.hash = hash;
+      this.key = key;
+      this.value = value;
+      this.next = next;
+    }
+
+    public final Long getKey() {
+      return key;
+    }
+
+    public final V getValue() {
+      return value;
+    }
+
+    public final String toString() {
+      return key + "=" + value;
+    }
+
+    public final int hashCode() {
+      return Objects.hashCode(key) ^ Objects.hashCode(value);
+    }
+
+    public final boolean equals(Object o) {
+      if (o == this)
+        return true;
+      if (o instanceof Node) {
+        Node<?> n = (Node<?>) o;
+        if (Objects.equals(key, n.getKey()) && Objects.equals(value, n.getValue())) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
 }
---
+++ src/test/java/de/comparus/opensource/longmap/LongMapImplTest.java
@@ -0,0 +1,261 @@
+package de.comparus.opensource.longmap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class LongMapImplTest {
+
+  @Test
+  public void testGetValueByKeyExpectedSameValue() {
+    LongMap<String> map = new LongMapImpl<>();
+    Long key = 1L;
+    String value = "value";
+
+    map.put(key, value);
+
+    Assert.assertNotNull(map.get(key));
+    Assert.assertEquals(value, map.get(key));
+  }
+
+  @Test
+  public void testPutValuesIntoMapExpectedAddedValuesAndKeysToMap() {
+    LongMap<String> map = new LongMapImpl<>();
+    Long firstKey = 1L;
+    String firstValue = "first";
+    Long secondKey = 2L;
+    String secondValue = "second";
+
+    map.put(firstKey, firstValue);
+    map.put(secondKey, secondValue);
+
+    Assert.assertNotNull(map.get(firstKey));
+    Assert.assertEquals(firstValue, map.get(firstKey));
+
+    Assert.assertNotNull(map.get(secondKey));
+    Assert.assertEquals(secondValue, map.get(secondKey));
+  }
+
+  @Test
+  public void testPutValuesWithSameKeyExpectedValueUpdate() {
+    LongMap<String> map = new LongMapImpl<>();
+    Long key = 1L;
+    String firstValue = "first";
+    String secondValue = "second";
+
+    map.put(key, firstValue);
+
+    Assert.assertNotNull(map.get(key));
+    Assert.assertEquals(firstValue, map.get(key));
+
+    map.put(key, secondValue);
+
+    Assert.assertNotNull(map.get(key));
+    Assert.assertEquals(secondValue, map.get(key));
+  }
+
+  @Test
+  public void testMapCapacityExtendsExpectedNewTableSize() {
+    LongMapImpl<String> map = new LongMapImpl<>(1);
+    Assert.assertTrue(map.getTableForTest().length == 1);
+
+    Long firstKey = 1L;
+    String firstValue = "first";
+    Long secondKey = 2L;
+    String secondValue = "second";
+
+    map.put(firstKey, firstValue);
+    Assert.assertTrue(map.getTableForTest().length == map.size() * 2);
+
+    map.put(secondKey, secondValue);
+    Assert.assertTrue(map.getTableForTest().length == map.size() * 2);
+  }
+
+  @Test
+  public void testMapLoadFactorExpectedNewTableSize() {
+    LongMapImpl<String> map = new LongMapImpl<>(4, 0.5f);
+
+    Assert.assertTrue(map.getTableForTest().length == 4);
+
+    Long firstKey = 1L;
+    String firstValue = "first";
+    Long secondKey = 2L;
+    String secondValue = "second";
+
+    map.put(firstKey, firstValue);
+    Assert.assertTrue(map.getTableForTest().length == 4);
+
+    map.put(secondKey, secondValue);
+    Assert.assertTrue(map.getTableForTest().length == 8);
+  }
+
+  @Test
+  public void testGetAllKeysExpectedArrayOfKeys() {
+    LongMap<String> map = new LongMapImpl<>();
+
+    long[] keys = {2000L, 23456L, 231L, 8L, 6872L, 5656L, 233L, 234L, 876L, 5454L};
+
+    map.put(231L, "zero");
+    map.put(234L, "one");
+    map.put(5454L, "two");
+    map.put(5656L, "three");
+    map.put(6872L, "four");
+    map.put(233L, "five");
+    map.put(876L, "six");
+    map.put(23456L, "seven");
+    map.put(8L, "eigth");
+    map.put(2000L, "nine");
+
+    long[] result = map.keys();
+
+    System.out.println(Arrays.toString(result));
+
+    Assert.assertArrayEquals(keys, result);
+  }
+
+  @Test
+  public void testGetAllValuesExpectedArrayofValues() {
+    LongMap<String> map = new LongMapImpl<>();
+
+    String[] values =
+        {"nine", "zero", "one", "two", "three", "four", "five", "six", "seven", "eigth"};
+
+    map.put(0L, "zero");
+    map.put(1L, "one");
+    map.put(2L, "two");
+    map.put(3L, "three");
+    map.put(4L, "four");
+    map.put(5L, "five");
+    map.put(6L, "six");
+    map.put(7L, "seven");
+    map.put(8L, "eigth");
+    map.put(2000L, "nine");
+
+    String[] result = map.values();
+
+    Assert.assertArrayEquals(values, result);
+  }
+
+  @Test
+  public void testRemoveExpectedKeyAndValueRemoved() {
+    LongMap<String> map = new LongMapImpl<>();
+
+    Long firstKey = 1L;
+    String firstValue = "first";
+    Long secondKey = 2L;
+    String secondValue = "second";
+
+    map.put(firstKey, firstValue);
+    map.put(secondKey, secondValue);
+
+    long mapSizeBeforeRemove = map.size();
+
+    Assert.assertEquals(firstValue, map.get(firstKey));
+    Assert.assertEquals(secondValue, map.get(secondKey));
+
+    map.remove(firstKey);
+
+    long mapSizeAfterRemove = map.size();
+
+    Assert.assertTrue(mapSizeAfterRemove == (mapSizeBeforeRemove - 1));
+    Assert.assertNull(map.get(firstKey));
+  }
+
+  @Test
+  public void testIsEmptyExpectedTrueWhenMapIsEmpty() {
+    LongMap<String> map = new LongMapImpl<>();
+    Assert.assertTrue(map.isEmpty());
+
+    map.put(1L, "Value");
+    Assert.assertFalse(map.isEmpty());
+
+    map.remove(1L);
+    Assert.assertTrue(map.isEmpty());
+  }
+
+  @Test
+  public void testClearExpectedExpectedIsEmptyTrue() {
+    LongMap<String> map = new LongMapImpl<>();
+
+    map.put(1L, "Value");
+    map.put(2L, "Value");
+    map.put(3L, "Value");
+
+    map.clear();
+    Assert.assertTrue(map.isEmpty());
+  }
+
+  @Test
+  public void testContainsValueExpectedTrueIfValueExist() {
+    LongMap<String> map = new LongMapImpl<>();
+
+    String[] values =
+        {"zero", "one", "two", "three", "four", "five", "six", "seven", "eigth", "nine"};
+
+    map.put(0L, values[0]);
+    map.put(1L, values[1]);
+    map.put(2L, values[2]);
+    map.put(3L, values[3]);
+    map.put(4L, values[4]);
+    map.put(5L, values[5]);
+    map.put(6L, values[6]);
+    map.put(7L, values[7]);
+    map.put(8L, values[8]);
+    map.put(2000L, values[9]);
+    map.put(10L, null);
+
+    Assert.assertTrue(map.containsValue(values[9]));
+    Assert.assertTrue(map.containsValue(null));
+    Assert.assertFalse(map.containsValue("WrongValue"));
+  }
+
+  @Test
+  public void testContainsKeyExpectedTrueifValueExist() {
+    LongMap<String> map = new LongMapImpl<>();
+
+    long[] keys = {0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 2000L};
+
+    map.put(keys[0], "value");
+    map.put(keys[1], "value");
+    map.put(keys[2], "value");
+    map.put(keys[3], "value");
+    map.put(keys[4], "value");
+    map.put(keys[5], "value");
+    map.put(keys[6], "value");
+    map.put(keys[7], "value");
+    map.put(keys[8], "value");
+    map.put(keys[9], "value");
+
+    Assert.assertTrue(map.containsKey(keys[9]));
+    Assert.assertFalse(map.containsKey(1000L));
+  }
+
+  @Test
+  public void testSequenceOfElementExpectedSortedArray() {
+    LongMap<String> map = new LongMapImpl<>();
+
+    long[] keys = {0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L};
+
+    map.put(keys[8], "value");
+    map.put(keys[9], "value");
+    map.put(keys[5], "value");
+    map.put(keys[0], "value");
+    map.put(keys[1], "value");
+    map.put(keys[2], "value");
+    map.put(keys[3], "value");
+    map.put(keys[4], "value");
+    map.put(keys[5], "value");
+    map.put(keys[6], "value");
+    map.put(keys[7], "value");
+
+    System.out.println(map);
+
+    long[] result = map.keys();
+
+    Assert.assertArrayEquals(keys, result);
+  }
+}
