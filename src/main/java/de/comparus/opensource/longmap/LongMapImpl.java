625bb6b9d57d454f9869467992dc6f0c5d01f475
Severyn Zabara <zabara.severyn@gmail.com>
2023-07-10 18:03:15 +0300

    Implemented functionality of LongMapImpl and added tests

--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -1,43 +1,264 @@
 package de.comparus.opensource.longmap;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongMapImpl<V> implements LongMap<V> {
-    public V put(long key, V value) {
-        return null;
-    }
 
-    public V get(long key) {
-        return null;
-    }
+	private static final int DEFAULT_CAPACITY = 16;
+	private static final float DEFAULT_LOAD_FACTOR = 0.75f;
 
-    public V remove(long key) {
-        return null;
-    }
+	private int capacity;
+	private float loadFactor;
+	private int size;
+	private Entry<V>[] buckets;
+	long[] keysCache = null;
+	V[] valuesCache = null;
 
-    public boolean isEmpty() {
-        return false;
-    }
+	Class<V> type = (Class<V>) Object.class;
 
-    public boolean containsKey(long key) {
-        return false;
-    }
+	public LongMapImpl() {
+		this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR, null);
+	}
 
-    public boolean containsValue(V value) {
-        return false;
-    }
+	public LongMapImpl(Class<V> type) {
+		this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR, type);
+	}
 
-    public long[] keys() {
-        return null;
-    }
+	public LongMapImpl(int capacity, float loadFactor, Class<V> type) {
+		if (capacity <= 0) {
+			throw new IllegalArgumentException(
+					"Capacity can not be less zero." + System.lineSeparator() + "Invalid capacity: " + capacity);
+		}
+		if (loadFactor <= 0 || loadFactor > 1) {
+			throw new IllegalArgumentException("The value of the load factor can only be in the range of 0 to 1"
+					+ System.lineSeparator() + "Invalid load factor: " + loadFactor);
+		}
 
-    public V[] values() {
-        return null;
-    }
+		this.type = type;
+		this.capacity = capacity;
+		this.loadFactor = loadFactor;
+		this.size = 0;
+		this.buckets = new Entry[capacity];
+	}
 
-    public long size() {
-        return 0;
-    }
+	public V put(long key, V value) {
+		keysCache = null;
+		valuesCache = null;
+		if (type == null || type == Object.class) {
+			type = (Class<V>) value.getClass();
+		} else if (value.getClass() != type) {
+			throw new IllegalArgumentException(
+					"Invalid value type. Available only " + type.getName() + "values for inserting");
+		}
+		int bucketIndex = getBucketIndex(key, capacity);
+		Entry<V> bucketEntry = buckets[bucketIndex];
+		if (bucketEntry != null) {
+			Entry<V> lastNotNullEntry = null;
+			while (bucketEntry != null) {
+				if (bucketEntry.getKey() == key) {
+					V prevValue = bucketEntry.getValue();
+					lastNotNullEntry.next = bucketEntry;
+					bucketEntry.setValue(value);
+					return prevValue;
+				}
+				lastNotNullEntry = bucketEntry;
+				bucketEntry = bucketEntry.next;
 
-    public void clear() {
+			}
+			lastNotNullEntry.next = new Entry<V>(key, value);
 
-    }
+		} else {
+			buckets[bucketIndex] = new Entry<V>(key, value);
+		}
+		size++;
+
+		if (size > capacity * loadFactor) {
+			resize();
+		}
+		return null;
+	}
+
+	public V get(long key) {
+		int bucketIndex = getBucketIndex(key, capacity);
+		Entry<V> bucketEntry = buckets[bucketIndex];
+		if (bucketEntry == null) {
+			return null;
+		} else {
+			Entry<V> prevEntry = null;
+			while (bucketEntry != null) {
+				prevEntry = bucketEntry;
+				bucketEntry = prevEntry.next;
+				if (bucketEntry == null) {
+					return null;
+				}
+				if (bucketEntry.getKey() == key) {
+					return bucketEntry.getValue();
+				} else {
+					return null;
+				}
+
+			}
+		}
+		return null;
+	}
+
+	public V remove(long key) {
+		keysCache = null;
+		valuesCache = null;
+		int bucketIndex = getBucketIndex(key, capacity);
+		Entry<V> bucketEntry = buckets[bucketIndex];
+		if (bucketEntry == null) {
+			return null;
+		} else if (bucketEntry.getKey() == key) {
+			V value = bucketEntry.getValue();
+			buckets[bucketIndex] = null;
+			size--;
+			return value;
+		} else {
+			Entry<V> prevEntry = null;
+			while (bucketEntry != null) {
+				prevEntry = bucketEntry;
+				bucketEntry = prevEntry.next;
+				if (bucketEntry.getKey() == key) {
+					Entry<V> nextEntry = bucketEntry.next;
+					prevEntry.next = nextEntry;
+					size--;
+					return bucketEntry.getValue();
+				}
+			}
+		}
+		return null;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public boolean containsKey(long key) {
+		int bucketIndex = getBucketIndex(key, capacity);
+		Entry<V> bucketEntry = buckets[bucketIndex];
+		if (bucketEntry != null) {
+			while (bucketEntry != null) {
+				if (bucketEntry.getKey() == key) {
+					return true;
+				}
+				bucketEntry = bucketEntry.next;
+			}
+		}
+		return false;
+	}
+
+	public boolean containsValue(V value) {
+		V[] values = values();
+		return Arrays.asList(values).contains(value);
+	}
+
+	public long[] keys() {
+		if (keysCache != null) {
+			return keysCache;
+		}
+		Set<Long> keySet = new HashSet<Long>();
+		for (Entry<V> entry : buckets) {
+			if (entry != null) {
+				while (entry != null) {
+					keySet.add(entry.getKey());
+					entry = entry.next;
+
+				}
+			}
+		}
+
+		long[] keys = new long[keySet.size()];
+		int i = 0;
+		for (long key : keySet) {
+			keys[i++] = key;
+		}
+		keysCache = keys;
+		return keys;
+	}
+
+	@SuppressWarnings("unchecked")
+	public V[] values() {
+		if (valuesCache != null) {
+			return valuesCache;
+		}
+		int i = 0;
+		V[] values = (V[]) Array.newInstance(type, size);
+		for (Entry<V> entry : buckets) {
+			if (entry != null) {
+				while (entry != null) {
+					values[i] = entry.getValue();
+					i++;
+					entry = entry.next;
+
+				}
+			}
+		}
+		valuesCache = values;
+		return values;
+	}
+
+	public long size() {
+		return size;
+	}
+
+	public void clear() {
+		size = 0;
+		buckets = new Entry[capacity];
+	}
+
+	private static class Entry<V> {
+		private long key;
+		private V value;
+
+		private Entry<V> next;
+
+		public Entry(long key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		public long getKey() {
+			return key;
+		}
+
+		public V getValue() {
+			return value;
+		}
+
+		public void setValue(V value) {
+			this.value = value;
+		}
+	}
+
+	private void resize() {
+		int newCapacity = capacity + capacity / 2;
+		if (newCapacity < 0) {
+			newCapacity = Integer.MAX_VALUE;
+		}
+		Entry<V>[] newBuckets = new Entry[newCapacity];
+
+		for (Entry<V> bucket : buckets) {
+			while (bucket != null) {
+				int newBucketIndex = getBucketIndex(bucket.getKey(), newCapacity);
+				Entry<V> newBucket = newBuckets[newBucketIndex];
+				if (newBucket == null) {
+					newBuckets[newBucketIndex] = bucket;
+				} else {
+					newBucket.next = bucket;
+				}
+				bucket = bucket.next;
+			}
+		}
+
+		capacity = newCapacity;
+		buckets = newBuckets;
+	}
+
+	private int getBucketIndex(long key, int capacity) {
+		return ((int) Math.abs(key) % capacity);
+	}
 }
---
+++ src/test/java/de/comparus/opensource/longmap/LongMapImplTest.java
@@ -0,0 +1,149 @@
+package de.comparus.opensource.longmap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LongMapImplTest {
+
+	private LongMapImpl<Integer> longMapImpl = new LongMapImpl<Integer>();
+
+	@BeforeEach
+	void fillMap() {
+		int i = -500_000;
+		while (i < 500_000) {
+			longMapImpl.put(i, i);
+			i++;
+		}
+	}
+
+	@Test
+	void testPut() {
+		int j = 0;
+		int i = -500_000;
+		boolean isAllPutted = false;
+		while (j < 1_000_000) {
+			if (longMapImpl.get(i) != null) {
+				isAllPutted = true;
+				break;
+			}
+			j++;
+			i++;
+		}
+		assertTrue(isAllPutted);
+	}
+
+	@Test
+	void testGet() {
+		int j = 0;
+		int i = -500_000;
+		while (j < 1_000_000) {
+			longMapImpl.get(i);
+			j++;
+			i++;
+		}
+		assertEquals(longMapImpl.size(), j);
+	}
+
+	@Test
+	void testRemove() {
+		int j = 0;
+		int i = -500_000;
+		boolean isAllremoved = true;
+		while (j < 1_000_000) {
+			longMapImpl.remove(i);
+			if (longMapImpl.get(i) != null) {
+				isAllremoved = false;
+				break;
+			}
+			j++;
+			i++;
+		}
+		assertTrue(isAllremoved);
+	}
+
+	@Test
+	void testIsEmpty() {
+
+		assertFalse(longMapImpl.isEmpty());
+		longMapImpl.clear();
+		assertTrue(longMapImpl.isEmpty());
+	}
+
+	@Test
+	void testContainsKey() {
+		int i = -500_000;
+		boolean isContains = true;
+		while (i < 500_000) {
+			isContains = longMapImpl.containsKey(i);
+			if (!isContains) {
+				break;
+			}
+			i++;
+		}
+		assertTrue(isContains);
+	}
+
+	@Test
+	void testContainsValue() {
+		int i = -5000;
+		boolean isContains = true;
+		while (i < 5000) {
+			isContains = longMapImpl.containsValue(i);
+			if (!isContains) {
+				break;
+			}
+			i++;
+		}
+		assertTrue(isContains);
+	}
+
+	@Test
+	void testKeys() {
+		long[] expectedKeys = new long[1000000];
+		int i = -500_000;
+		int j = 0;
+		while (j < expectedKeys.length) {
+			expectedKeys[j] = i;
+			i++;
+			j++;
+		}
+		long[] actualKeys = longMapImpl.keys();
+		assertEquals(expectedKeys.length, actualKeys.length);
+	}
+
+	@Test
+	void testValues() {
+		Integer[] expectedValues = new Integer[1000000];
+		int i = -500_000;
+		int j = 0;
+		while (j < expectedValues.length) {
+			expectedValues[j] = i;
+			i++;
+			j++;
+		}
+
+		assertEquals(expectedValues.length, longMapImpl.values().length);
+	}
+
+	@Test
+	void testSize() {
+		int i = -500_000;
+		int j = 0;
+		while (j < 10) {
+			longMapImpl.remove(i);
+			i++;
+			j++;
+		}
+		assertTrue(longMapImpl.size() == longMapImpl.values().length);
+	}
+
+	@Test
+	void testClear() {
+		longMapImpl.clear();
+		assertTrue(longMapImpl.isEmpty());
+		assertTrue(longMapImpl.size() == 0);
+	}
+
+}
